9d0506833027005ecb14962fccd3639e9013d67e
francis <Francis365@outlook.com>
2024-09-11 22:38:22 +0100

    updated .gitignore

--- Vaults.Client/obj/Debug/net9.0/Vaults.Client.AssemblyInfo.cs
+++ Vaults.Client/obj/Debug/net9.0/Vaults.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaults.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0dce71ee65247b9f85f4c25e549f3534da94ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaults.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaults.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Vaults/obj/Debug/net9.0/Vaults.AssemblyInfo.cs
+++ Vaults/obj/Debug/net9.0/Vaults.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0dce71ee65247b9f85f4c25e549f3534da94ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
