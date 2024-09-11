6d0dce71ee65247b9f85f4c25e549f3534da94ea
francis <Francis365@outlook.com>
2024-09-11 22:27:19 +0100

    first commit

---
+++ Vaults.Client/Program.cs
@@ -0,0 +1,14 @@
+using Vaults.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
